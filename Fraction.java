a7f9d9b11c854a676c881d9af8f9d79591484eae
Rod Clarenz Mabalot <147684772+Renzo404@users.noreply.github.com>
2024-02-18 17:10:10 +0800

    + comments
    missing: call toDouble method

--- Fraction.java
+++ Fraction.java
@@ -1,5 +1,6 @@
 package prog2.prelimgroup;
 
+import java.text.DecimalFormat;
 public class Fraction {
     private int numerator;
     private int denominator;
@@ -32,69 +33,65 @@ public class Fraction {
         return (numerator + "/" + denominator);
     }
     public double toDouble(){
-        return numerator / denominator;
+        return (numerator / denominator);
     }
-    private int computeGCD(int num1, int  num2) {
-        //Euclidian Algorithm
-        //a is the greater number, b is the lesser number
-        int a,b;
-        a = Math.max(num1, num2);
-        b = Math.min(num1, num2);
+    public int computeGCD(int a, int b){
+        int num;    // acts as temp
 
-        int r = 0; //remainder
-
-        while (a % b != 0) {
-            r = a % b;
-            a = b; //the previous denominator of a/b will be the dividend
-            b = r; //the previous remainder will be the divisor
+        if (a < b){ // arranges values by size; larger value first
+            num = a;
+            a = b;
+            b = num;
         }
-
+        while (a % b != 0){ // uses Euclidean Algorithm
+            num = a % b;
+            a = b;
+            b = num;
+        }
         return b;
+    }
+    public String convertToDecimal(double num, double den){
+        DecimalFormat df = new DecimalFormat("#.##");   // formats decimal places to hundredths
+
+        double dec = num / den; // getting decimal value
+        return (df.format(dec));
     }
     public String add(int num1, int den1, int num2, int den2){
         int div = computeGCD(den1, den2);
-        int lcd = (den1 * den2) / div;
+        int lcd = (den1 * den2) / div;  // computes LCD
 
-        num1 *= (lcd / den1);
+        num1 *= (lcd / den1);   // converting numerators to match their denominator
         num2 *= (lcd / den2);
-        den1 = lcd;
-        den2 = lcd;
 
         int sumNum = num1 + num2;
-        double dec = (double) sumNum / lcd;
-
-        return (sumNum + "/" + lcd + " or " + dec);
+        return (sumNum + "/" + lcd + " or " + convertToDecimal(sumNum, lcd));   // displays in main method
     }
     public String subtract(int num1, int den1, int num2, int den2){
         int div = computeGCD(den1, den2);
-        int lcd = (den1 * den2) / div;
+        int lcd = (den1 * den2) / div;  // computes LCD
 
-        num1 *= (lcd / den1);
+        num1 *= (lcd / den1);   // converting numerators to match their denominator
         num2 *= (lcd / den2);
-        den1 = lcd;
-        den2 = lcd;
 
         int difNum = num1 - num2;
-        double dec = (double) difNum / lcd;     // need to make decimal converter method for "or" .2f option in results
-
-        return (difNum + "/" + lcd + " or " + dec);
+        return (difNum + "/" + lcd + " or " + convertToDecimal(difNum, lcd));   // displays in main method
     }
     public String multiply(int num1, int den1, int num2, int den2){
         int divisor = num1 * num2;
         int dividend = den1 * den2;
 
-        return (divisor + "/" + dividend);
+        return (divisor + "/" + dividend + " or " + convertToDecimal(divisor, dividend));   // displays in main method
     }
     public String divide(int num1, int den1, int num2, int den2){
         int divisor = num1 * den2;
-        int dividend = den1 * num1;
+        int dividend = den1 * num2;
 
-        return (divisor + "/" + dividend);
+        return (divisor + "/" + dividend + " or " + convertToDecimal(divisor, dividend));   // displays in main method
     }
     public String reduce(int num, int den){
         int gcd = computeGCD(num, den);
         int redNum = num / gcd;
         int redDen = den / gcd;
-        return redNum + "/" + redDen;
+        return redNum + "/" + redDen + " or " + convertToDecimal(redNum, redDen);   // displays in main method
     }
 }
