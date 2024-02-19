6977527d70ddcefa069a7c94b6c5723d1567205a
Portia Caramat <146707717+PortiaCaramat@users.noreply.github.com>
2024-02-19 09:45:45 +0800

    Add files via upload

---
+++ DivideFractions.java
@@ -0,0 +1,6 @@
+public String fractionDivide (int numerator, int denominator){
+    int divnum = numerator * denominator;
+    int dividend = denominator * numerator;
+
+    return divnum + "/" + dividend;
+}
