231cd3a1c3a4275483481f9d25c9450c960a86cb
Rod Clarenz Mabalot <147684772+Renzo404@users.noreply.github.com>
2024-03-22 19:29:35 +0800

    GUI-based try-catch statement

--- src/prog2/midgroup01/FractionTester.java
+++ src/prog2/midgroup01/FractionTester.java
@@ -1,51 +1,70 @@
 package prog2.midgroup01;
 import prog2.prelimgroup.Fraction;
 
-import java.util.Scanner;
+import javax.swing.*;
 
 public class FractionTester {
     public static void main(String[] args){
-        Scanner kbd= new Scanner(System.in);
-      
-        System.out.print("Fraction 1 numerator:");
-        int numFrac1= kbd.nextInt();
-        System.out.print("Fraction 1 denominator:");
-        int denFrac1 = kbd.nextInt();
-        System.out.print("Fraction 1 whole Number:");
-        int wholeFrac1 = kbd.nextInt();
-        System.out.print("Fraction 2 numerator:");
-        int numFrac2 = kbd.nextInt();
-        System.out.print("Fraction 2 denominator:");
-        int denFrac2 = kbd.nextInt();
-        System.out.print("Fraction 1 whole number:");
-        int wholeFrac2 = kbd.nextInt();
-
-
-        MixedFraction fraction1 = new MixedFraction(wholeFrac1, numFrac1, denFrac1);
-        MixedFraction fraction2 = new MixedFraction(wholeFrac2, numFrac2, denFrac2);
-        MixedFraction sum= fraction1.add(fraction2);
-        MixedFraction dif= fraction1.subtract(fraction2);
-        MixedFraction quotient= fraction1.divideBy(fraction2);
-        MixedFraction product = fraction1.multiplyBy(fraction2);
-
-
-        System.out.print("\n Addition: "+ fraction1.toString() +" + "+ fraction2.toString() +" = "+ sum.toString()+" or "+ sum.toDouble());
-        System.out.print("\n Subtraction: "+ fraction1.toString() +" - "+ fraction2.toString() +" = "+ dif.toString()+" or "+ dif.toDouble());
-        System.out.print("\n Division: "+ fraction1.toString() +" / "+ fraction2.toString() +" = "+ quotient.toString()+" or "+ quotient.toDouble());
-        System.out.print("\n Multiplication: "+ fraction1.toString() +" * "+ fraction2.toString() +" = "+ product.toString()+" or "+ product.toDouble());
-        System.out.print("\nFraction 1 whole number:"+ fraction1.getWhole());
-        System.out.print("\nFraction 1 numerator:"+ fraction1.getNumerator());
-        System.out.print("\nFraction 1 denominator:"+ fraction1.getDenominator());
+        //call GUI here
 
         try{
-            //code here
+            String whole1Str = num1Field.getText();     // numfield names to be updated after GUI
+            String num1Str = num12Field.getText();
+            String den1Str = num3Field.getText();
+            String whole2Str = num4Field.getText();
+            String num2Str = num5Field.getText();
+            String den2Str = num6Field.getText();
+            String operator = (String) operatorComboBox.getSelectedItem();
+
+            /* if user input is limited:
+            if (whole1Str.length() >= 3 || whole2Str.length() >= 3) {
+                JOptionPane.showMessageDialog(null, "Whole numbers cannot exceed three [3] digits!" +
+                        "\nPlease try again.", "Number Input Limit", JOptionPane.ERROR_MESSAGE);
+            } else if (num1Str.length() >= 5 || num2Str.length() >= 5 || den1Str.length() >= 5 || den2Str.length() >= 5) {
+                JOptionPane.showMessageDialog(null, "Numerators and denominators cannot exceed five [5] digits!" +
+                        "\nPlease try again.", "Number Input Limit", JOptionPane.ERROR_MESSAGE);
+            }
+             */
+
+            // convert user input strings to numbers
+            int wholeFrac1 = Integer.parseInt(whole1Str);
+            int numFrac1 = Integer.parseInt(num1Str);
+            int denFrac1 = Integer.parseInt(den1Str);
+            int wholeFrac2 = Integer.parseInt(whole2Str);
+            int numFrac2 = Integer.parseInt(num2Str);
+            int denFrac2 = Integer.parseInt(den2Str);
+
+            MixedFraction fraction1 = new MixedFraction(wholeFrac1, numFrac1, denFrac1);
+            MixedFraction fraction2 = new MixedFraction(wholeFrac2, numFrac2, denFrac2);
+
+            if (denFrac1 == 0 || denFrac2 == 0) {
+                JOptionPane.showMessageDialog(null, "Denominator cannot be zero!" +
+                        "\nPlease try again.", "ERROR", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            double result = 0;
+
+            // Performs the calculation based on the selected operator
+            switch (operator) {
+                case "+":
+                    MixedFraction sum= fraction1.add(fraction2);
+                    break;
+                case "-":
+                    MixedFraction difference= fraction1.subtract(fraction2);
+                    break;
+                case "*":
+                    MixedFraction product = fraction1.multiplyBy(fraction2);
+                    break;
+                case "/":
+                    MixedFraction quotient= fraction1.divideBy(fraction2);
+                    break;
+            }
         } catch (NumberFormatException numEx) {
             JOptionPane.showMessageDialog(null, "Please enter valid numbers!", "ERROR", JOptionPane.ERROR_MESSAGE);
-        } catch (ArithmeticException ariEx) {
-            JOptionPane.showMessageDialog(null, "Zero (0) cannot be a denominator! \nPlease try again.", "ERROR", JOptionPane.ERROR_MESSAGE);
+        } catch (ArithmeticException ariEx) {   // would be solved by input limits; could be removed
+            JOptionPane.showMessageDialog(null, "Output exceeded! \nPlease try again.", "ERROR", JOptionPane.ERROR_MESSAGE);
         } catch (Exception ex) {
             JOptionPane.showMessageDialog(null, "Oh no! It seems like we have encountered an error. \nPlease try again.", "ERROR", JOptionPane.ERROR_MESSAGE);
-            }
         }
     }
 
